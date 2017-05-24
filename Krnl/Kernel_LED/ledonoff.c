b9e16e47991cb3cf6b8a7679d965b70f65c18f87
Braindawgs <andrezdethklok@gmail.com>
2017-05-24 12:50:10 +0000

    Kernel msg deleted

--- Krnl/Kernel_LED/ledonoff.c
+++ Krnl/Kernel_LED/ledonoff.c
@@ -15,32 +15,14 @@ static struct gpio button = {10, GPIOF_IN, "GPIO_IN"};
 
 static int button_irq = -1;
 
-//static struct task_struct *ts = NULL;
-
-/*static int led_thread(void *data)
-{
-	printk(KERN_INFO "Entering LED thread");	
-
-	while(!kthread_should_stop())
-	{
-		if(gpio_get_value(button.gpio))
-		gpio_set_value(led.gpio,  1);
-		else 
-		gpio_set_value(led.gpio, 0);
-	}
-
-	printk(KERN_INFO "Exiting LED thread");
-	return 0;
-}*/
-
 irqreturn_t button_isr(int irq, void *data)
 	{
-		printk(KERN_INFO " Value %d",gpio_get_value(button.gpio));
-		printk(KERN_INFO "Entering IRQ Handler\r\n");	
+//		printk(KERN_INFO " Value %d",gpio_get_value(button.gpio));
+//		printk(KERN_INFO "Entering IRQ Handler\r\n");	
 		gpio_set_value(led.gpio, gpio_get_value(button.gpio));
 	/*	if(gpio_get_value(button.gpio))
 		gpio_set_value(led.gpio,1);
-		else 							
+		else
 		gpio_set_value(led.gpio,0);*/
 return IRQ_HANDLED;
 	}
@@ -49,9 +31,9 @@ return IRQ_HANDLED;
 /*MODULE INIT FUNCTION*/
 static int gpiomod_init(void)
 {
-	printk(KERN_INFO "Init Complete\r\n");
+	printk(KERN_ALERT "Init Complete\r\n");
 
-	int ret = 0;
+	int ret;
 
 	ret = gpio_request(17, "LED_OUT");
 		if(ret)
@@ -59,7 +41,7 @@ static int gpiomod_init(void)
 				printk(KERN_ERR "Unable to req GPIO -- LED_OUT\r\n");
 				return ret; 
 			}
-	
+
 	ret = gpio_request(10, "GPIO_IN");
 		if(ret)
 			{
@@ -75,10 +57,10 @@ static int gpiomod_init(void)
 				printk(KERN_ERR "Unable to req IRQ\n\r");	
 				gpio_free(10);	
 			}
-	
+
 	button_irq = ret;
 	printk(KERN_INFO "Req for IRQ complete\r\n");
-	
+
 	ret = request_irq(button_irq, button_isr, IRQF_TRIGGER_BOTH | IRQF_DISABLED, "GPIO_IN", NULL);	
 
 /*	ts = kthread_create(led_thread, NULL, "led_thread\r\n");
--- Krnl/Kernel_LED/ledonoff.mod.c
+++ Krnl/Kernel_LED/ledonoff.mod.c
@@ -25,9 +25,9 @@ __attribute__((section("__versions"))) = {
 	{ 0xc1c8e1d4, __VMLINUX_SYMBOL_STR(gpiod_direction_input) },
 	{ 0xd5f4eaf7, __VMLINUX_SYMBOL_STR(gpiod_direction_output_raw) },
 	{ 0x47229b5c, __VMLINUX_SYMBOL_STR(gpio_request) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0x2e5810c6, __VMLINUX_SYMBOL_STR(__aeabi_unwind_cpp_pr1) },
 	{ 0x64f8b539, __VMLINUX_SYMBOL_STR(gpiod_set_raw_value) },
-	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0x717905ad, __VMLINUX_SYMBOL_STR(gpiod_get_raw_value) },
 	{ 0xcd387ee9, __VMLINUX_SYMBOL_STR(gpio_to_desc) },
 	{ 0xb1ad28e0, __VMLINUX_SYMBOL_STR(__gnu_mcount_nc) },
@@ -39,4 +39,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "7AE3D49072B278E737CAECD");
+MODULE_INFO(srcversion, "B768A454D77A6B398573BE2");
